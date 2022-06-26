830b2cfce9222110320a3ff7b72f8196b414e67c
sd12354 <78817767+sd12354@users.noreply.github.com>
2022-06-26 15:18:12 -0400

    e

--- Syntis/src/main/java/com/syntis/util/Util.java
+++
@@ -1,9 +0,0 @@
-package com.syntis.util;
-
-import org.apache.commons.lang3.builder.ToStringBuilder;
-
-public class Util {
-	public static void printBean(Object bean) {
-		System.out.println(ToStringBuilder.reflectionToString(bean));
-	}
-}
--- Syntis/src/main/webapp/resources/js/controllers/IndexController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function IndexController($scope, $http){
-	$scope.index = function(){
-		console.log("----- index.js (doesn't run here?) -----");
-	};
-	$scope.index();
-}
