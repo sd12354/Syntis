8a13f9aa7a34eae0bf1d450caf9b1bbaf70d6a5a
sd12354 <78817767+sd12354@users.noreply.github.com>
2022-06-28 18:02:36 -0400

    change

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function minimize() {
-  var item = document.getElementById
+function minimize(clickID) {
+  console.log(clickID);
+  var paragraphID = clickID + "Bio";
+  console.log(paragraphID)
+  if(document.getElementById(paragraphID).style.display = "block") {
+    document.getElementById(paragraphID).style.display = "none";
+  }
 }
+
+var typed = new Typed('.topTxt', {
+  strings: ["A New Class of Threapy"],
+  typeSpeed: 50,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+});
