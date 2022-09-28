9b533282d184c01b841dcaec663dc564d38bfda4
sd12354 <78817767+sd12354@users.noreply.github.com>
2022-09-27 20:26:48 -0400

    gg

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function minimize(clickID) {
-  console.log(clickID);
   var paragraphID = clickID + "Bio";
   var text = document.getElementById(paragraphID)
-  console.log(paragraphID)
-  if (text.style.display === "none") {
-    text.style.display = "block";
-    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/remove.svg')"
-  } else {
-    text.style.display = "none"
+  if (text.style.display === "block") {
+    text.style.display = "none";
     document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/add.svg')"
+  } else {
+    text.style.display = "block"
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/remove.svg')"
   }
 }
 
@@ -21,3 +19,43 @@ var typed = new Typed('.topTxt', {
 });
 
 
+
+document.getElementById("gio").addEventListener("click", (e) => {
+  var paragraphID = clickID + "Bio";
+  var text = document.getElementById(paragraphID)
+  console.log(paragraphID)
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/remove.svg')"
+  } else {
+    text.style.display = "none"
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/add.svg')"
+  }
+
+})
+document.getElementById("bob").addEventListener("click", (e) => {
+  console.log(clickID);
+  var paragraphID = clickID + "Bio";
+  var text = document.getElementById(paragraphID)
+  console.log(paragraphID)
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/remove.svg')"
+  } else {
+    text.style.display = "none"
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/add.svg')"
+  }
+})
+document.getElementById("rahul").addEventListener("click", (e) => {
+  console.log(clickID);
+  var paragraphID = clickID + "Bio";
+  var text = document.getElementById(paragraphID)
+  console.log(paragraphID)
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/remove.svg')"
+  } else {
+    text.style.display = "none"
+    document.getElementById(clickID).style.backgroundImage = "url('/assets/svg/add.svg')"
+  }
+})
